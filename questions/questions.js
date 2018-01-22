0c768d9f6e638aee8cef6d2b14eec22a4a00df78
thyagajan <thyagajan@gmail.com>
2018-01-22 15:06:48 +0530

    Create questions.js

---
+++ questions/questions.js
@@ -0,0 +1,35 @@
+questions=[
+				{
+					"id":1,
+					"qType":"mChoice", 
+					"question":"What is the name of Watson powered Humanioid Robot?",
+					"option1":"Pepper",
+					"option2":"Peppa",
+					"option3":"Walto",
+					"option4":"Walter",
+					"answer":"Pepper"
+				},
+				{
+					"id":2,
+					"qType":"mChoice", 
+					"question":"What is IBM's first president Name?",
+					"option1":"Watson",
+					"option2":"Jerold",
+					"option3":"Sam Palmisano",
+					"option4":"Hendry G",
+					"answer":"Watson"
+				},
+				{
+					"id":3,
+					"qType":"noChoice", 
+					"question":" What begins with T, ends with T and has T in it?",
+					"answer":"Teapot",
+					"points":0,
+					"passPoints":0,
+					"allowPass":false
+				}
+			];
+
+points={"points":10,"delayedPoints":5,"passPoints":2};
+allowPass = true;
+delayChoice=true;
