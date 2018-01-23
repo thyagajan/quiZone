139f5f506761e11fe1535814d2f6fd9e80e7598f
thyagajan <thyagajan@gmail.com>
2018-01-24 01:23:03 +0530

    added questions

--- questions/questions.js
+++ questions/questions.js
@@ -2,7 +2,7 @@ questions=[
 				{
 					"id":1,
 					"qType":"mChoice", 
-					"question":"What is the name of Watson powered Humanioid Robot?",
+					"question":"What is the name of IBM's supercomputer powered Humanioid Robot?",
 					"option1":"Pepper",
 					"option2":"Peppa",
 					"option3":"Walto",
@@ -12,22 +12,130 @@ questions=[
 				{
 					"id":2,
 					"qType":"mChoice", 
-					"question":"What is IBM's first president Name?",
-					"option1":"Watson",
-					"option2":"Jerold",
-					"option3":"Sam Palmisano",
-					"option4":"Hendry G",
-					"answer":"Watson"
+					"question":"who is the founder of IBM?",
+					"option1":"Thomas J.Watson",
+					"option2":"Frank T.Cary",
+					"option3":"David Duffield",
+					"option4":"Charles Flint",
+					"answer":"Charles Flint"
 				},
+				
 				{
 					"id":3,
+					"qType":"mChoice", 
+					"question":"what is the speed of T3 line in mbps?",
+					"option1":"1.5",
+					"option2":"4.5",
+					"option3":"15",
+					"option4":"45",
+					"answer":"45"
+				},
+				{
+					"id":4,
+					"qType":"mChoice", 
+					"question":"How many /30 blocks can be obtained from a /24 IP block?",
+					"option1":"30",
+					"option2":"32",
+					"option3":"16",
+					"option4":"64",
+					"answer":"64"
+				},
+				{
+					"id":5,
 					"qType":"noChoice", 
-					"question":" What begins with T, ends with T and has T in it?",
-					"answer":"Teapot",
+					"question":" It goes all over the world, but always stays in a corner. What is that??",
+					"answer":"Stamp",
 					"points":0,
 					"passPoints":0,
 					"allowPass":false
-				}
+				},
+				{
+					"id":6,
+					"qType":"noChoice", 
+					"question":" I'm the most slippery country in the world. Tell who am I??",
+					"answer":"Greece",
+					"points":0,
+					"passPoints":0,
+					"allowPass":false
+				},
+				{
+					"id":7,
+					"qType":"mChoice", 
+					"question":"Who is the CEO and President of AT&T?",
+					"option1":"John Donovan",
+					"option2":"Randall L. Stephenson",
+					"option3":"John Stankey",
+					"option4":"John J. Stephens",
+					"answer":"Randall L. Stephenson"
+				},
+				{
+					"id":8,
+					"qType":"mChoice", 
+					"question":"Where is AT&T head quarters located(City Name)?",
+					"option1":"San Antonio, Texas",
+					"option2":"Houstan, Texas",
+					"option3":"Austin, Texas",
+					"option4":"Dallas, Texas",
+					"answer":"Dallas, Texas"
+				},
+				{
+					"id":9,
+					"qType":"mChoice", 
+					"question":"which configlet type removes the line 'no shutdown' from the interface(in 	LPP)?",
+					"option1":"IPC",
+					"option2":"IBC",
+					"option3":"TTU",
+					"option4":"RC",
+					"answer":"TTU"
+				},
+				{
+					"id":10,
+					"qType":"mChoice", 
+					"question":"Expand ICORE?",
+					"option1":"Internet Common Operations Resource and Engineering",
+					"option2":"Interspan Common Operations Resource and Engineering",
+					"option3":"Interactive Common Operations Resource and Engineering",
+					"option4":"Interops Common Operations Resource and Engineering ",
+					"answer":"Interspan Common Operations Resource and Engineering"
+				},
+				{
+					"id":11,
+					"qType":"noChoice", 
+					"question":"In an empty land a man was found dead wearing his back pack, How could have he died?",
+					"answer":"His Parachute failed to open",
+					"points":0,
+					"passPoints":0,
+					"allowPass":false
+				},
+				{
+					"id":12,
+					"qType":"noChoice", 
+					"question":"A man went to a party and drank some of the punch. He then left early. Everyone at the party who drunk the punch subsequently died of poisioning. Why did the man not die?",
+					"answer":"Poison came from the ice cubes.",
+					"points":0,
+					"passPoints":0,
+					"allowPass":false
+				},
+				{
+					"id":13,
+					"qType":"mChoice", 
+					"question":"In 1924 IBM was renamed from the orginal name which was founded in 1911,what was the original name?" ,
+					"option1":"Computing-Transporting-Reviewing Company",
+					"option2":"Computing-Tabulating-Reviewing Company",
+					"option3":"Computing-Tabulating-Recording Company",
+					"option4":"Computing-Transporting-Recording Company",
+					"answer":"Computing-Tabulating-Recording Company"
+				},
+				{
+					"id":14,
+					"qType":"mChoice", 
+					"question":"What is the name of IBM's supercomputer that won the chess match against a world chess champion in 1997?" ,
+					"option1":"Big Blue",
+					"option2":"Deep Blue",
+					"option3":"Blue Queen",
+					"option4":"Blue Fish",
+					"answer":"Deep Blue"
+				},
 			];
 
 points={"points":10,"delayedPoints":5,"passPoints":2};
